8fd99861f2a9c9e5435370a744903bfa4ae7c23f
Chri2704 <angilerichristian439@gmail.com>
2023-06-11 15:41:43 +0200

    manage account

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -33,7 +33,32 @@
 
                 @auth
                     <li class="navbar-spaced">
-                        <a class="btn btn-primary" href="{{route('dashboard')}}">{{Auth::user()->name }}</a>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary bg-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a href="{{route('dashboard')}}">{{Auth::user()->name }}</a>
+                            </button>     
+                        <div class="dropdown-content">
+                        <x-slot name="content">
+                            <!-- Account Management -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Account') }}
+                            </div>
+
+                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                {{ __('Profile') }}
+                            </x-jet-dropdown-link>
+                            <div class="border-t border-gray-100"></div>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
+
+                                <x-jet-dropdown-link href="{{ route('logout') }}"
+                                         @click.prevent="$root.submit();">
+                                    {{ __('Log Out') }}
+                                </x-jet-dropdown-link>
+                        </div>
+</div>
                     </li>
                 @else
                     <li class="navbar-spaced">
