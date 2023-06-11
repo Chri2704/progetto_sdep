4cf8c09d857ac75cd4b82e7e909ea64bfb919f82
Chri2704 <angilerichristian439@gmail.com>
2023-06-11 17:01:33 +0200

    è

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -37,26 +37,27 @@
                             <button class="btn btn-secondary bg-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <a href="{{route('dashboard')}}">{{Auth::user()->name }}</a>
                             </button>     
-                        <div class="dropdown-content">
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                            <div class="dropdown-content">
+                                <x-slot name="content">
+                                    <!-- Account Management -->
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                    </div>
 
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-jet-dropdown-link>
-                            <div class="border-t border-gray-100"></div>
+                                    <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                        {{ __('Profile') }}
+                                    </x-jet-dropdown-link>
+                                    <div class="border-t border-gray-100"></div>
 
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}" x-data>
+                                        @csrf
 
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-jet-dropdown-link>
+                                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                            @click.prevent="$root.submit();">
+                                        {{ __('Log Out') }}
+                                        </x-jet-dropdown-link>
+                                    </form>
                                 
                         </div>
 </div>
