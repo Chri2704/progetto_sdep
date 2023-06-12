9ccfa4ff05eace3b4a957d9f9a11e34595938902
Cassio7 <acassieri@gmail.com>
2023-06-12 18:11:18 +0200

    db

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -19,7 +19,7 @@ class Admin extends Seeder
             'name' => 'Admin',
             'email' => 'admin@tad.apd.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',//password
             'remember_token' => Str::random(10),
             'admin' => 1,
         ]);
@@ -39,7 +39,7 @@ class Amico extends Seeder
             'name' => 'massimo',
             'email' => 'massimo@visconti.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',//password
             'remember_token' => Str::random(10),
             'admin' => 0,
         ]);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // per fare il seed devo fare php artisan migrate:fresh --seed
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
         $this->call(Admin::class);
         $this->call(Amico::class);
         //$this->call(TortDyb::class); //aggiunge il tortellino alla dybala
