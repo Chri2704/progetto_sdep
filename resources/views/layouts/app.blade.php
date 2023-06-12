d2e16972a74db0f76b773fd416b3350d20e79fd9
Cassio7 <acassieri@gmail.com>
2023-06-12 16:30:58 +0200

    aggiornato route e tap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Tad&Apd</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
