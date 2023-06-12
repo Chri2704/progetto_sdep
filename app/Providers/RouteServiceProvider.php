bd69ff819d24f6297f67e3e878489051ee103a95
Cassio7 <acassieri@gmail.com>
2023-06-12 17:14:39 +0200

    hide dashboad for not admin users

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/';
+    public const DASH = '/dashboard'; //nuova costante per andare in dashboard dopo login
 
     /**
      * The controller namespace for the application.
