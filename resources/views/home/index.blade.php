536b0737f7a4c1c3295da23f66349743a0091e7a
Chri2704 <angilerichristian439@gmail.com>
2023-06-13 19:14:37 +0200

    home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,2 +1,67 @@
 <x-guest-layout>
 </x-guest-layout>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" style="display:flex">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/images/logo.jpg" class="w-100 tortellini " loading="">
+      <div class="carousel-caption d-none d-md-block contando">
+      <button type="button" class="btn btn-secondary bg-secondary btn-lg"><a class="container" href="catalogo">Scopri di più</a></button>
+        <!-- <h5 class="giga boldi">Contattaci!</h5>
+        <p class="giga ">spediamo in tutta Italia</p> -->
+      </div>
+
+    </div>
+    <div class="carousel-item">
+      <img src="/images/tortellini_romagnoli.jpg" class=" w-100 tortelli" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <!-- <h5 class="giga boldi">Contattaci!</h5>
+        <p class="giga ">spediamo in tutta Italia</p> -->
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/images/tortellini.jpg" class="w-100 tortelli" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+<div class="">
+  <div class="row">
+    <div class="col">
+      <img src="/images/ricetta.jpg" class="foto_sotto zoom-image" alt="">
+    </div>
+    <div class="col">
+        <img src="/images/ranocchia.jpg" class="foto_sotto zoom-image"  alt="">
+    </div>
+
+  </div>
+</div>
+
+<!-- <div class="image-container">
+    <div class="image-container__item">
+        <img src="/images/ricetta.jpg" class="zoom-image" alt="">
+        <button class="image-container__button">Pulsante</button>
+    </div>
+
+    <div class="image-container__item">
+        <img src="/images/ranocchia.jpg" class="zoom-image" alt="">
+        <button class="image-container__button">Pulsante</button>
+    </div>
+</div>
+
+<div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>ciao
+</div> -->
