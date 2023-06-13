3f9455bfde287aac6d0ba6472f9b35a9a97b70a4
Cassio7 <acassieri@gmail.com>
2023-06-13 19:39:42 +0200

    carrello delete not complete

--- resources/views/carrello.blade.php
+++ resources/views/carrello.blade.php
@@ -1,32 +1,35 @@
 <x-guest-layout>
 </x-guest-layout>
+<h1 class="fs-1" style="text-align: center;">Carrello</h1> 
 <!-- permette di stampare gli ordini e relative informazioni al prodotto,
 dati passati tramite Homecontroller, a differenza di catalogo li stampo con -> invece che [] -->
 <table class="table">
     <thead>
         <tr>
-            <th scope="col">Id ordine</th>
+            <th scope="col" style="text-align: center;">Id ordine</th>
             <th scope="col">Immagine</th>
-            <th scope="col">Id prodotto</th>
+            <th scope="col" style="text-align: center;">Id prodotto</th>
             <th scope="col">Prodotto</th>
             <th scope="col">Descrizione</th>
             <th scope="col">Quantità</th>
-            <th scope="col">Prezzo unitario</th>
+            <th scope="col" style="text-align: center;">Prezzo unitario</th>
             <th scope="col">Totale</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($orders as $order)
         <tr>
-            <th scope="row">{{$order->id}}</th>
+            <th scope="row">#{{$order->id}}</th>
             <td><img src="{{ asset('images/db/'.$order->image)}}" alt="Immagine" class="centerimg"></td>
-            <td>{{$order->product_id}}</td>
+            <td>#{{$order->product_id}}</td>
             <td>{{$order->nome_prodotto}}</td>
             <td>{{$order->descrizione}}</td>
-            <td>{{$order->quantity}}</td>
-            <td>{{$order->prezzo}}</td>
-            <td>{{$order->prezzo * $order->quantity}}</td>
-            <td><button type="button" class="btn btn-outline-danger">Cancella</button></td>
+            <td style="text-align: center;">{{$order->quantity}}</td>
+            <td style="text-align: center;">{{$order->prezzo}}€</td>
+            <td style="text-align: center;">{{$order->prezzo * $order->quantity}}€</td>
+            <td>
+                <button type="button" class="btn btn-outline-danger" name="{{$order->id}}">Cancella</button>
+            </td>
         </tr>
         @endforeach
     </tbody>
@@ -37,4 +40,4 @@ dati passati tramite Homecontroller, a differenza di catalogo li stampo con -> i
 
     @endforeach
 </label>
-<button type="button" class="btn btn-outline-success">Success</button>
+<button type="button" class="btn btn-outline-success">Acquista</button>
