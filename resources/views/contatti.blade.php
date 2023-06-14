95270f04019492a75d2063199610b24031a07b60
Chri2704 <angilerichristian439@gmail.com>
2023-06-14 10:36:17 +0200

    contatti

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,3 +1,41 @@
 <x-guest-layout>
 </x-guest-layout>
-<h1 class="fs-1" style="text-align: center;">Contatti azienda</h1>
+<br><br>
+<h1 class="fs-1 center" style="text-align: center;">About us</h1>
+<br><br><br>
+<div class="contatti fs-3" style="text-align: justify;">
+    <p>
+        Benvenuti nella pagina dei contatti dell'azienda Tad&Apd con sede a Roma.<br>
+        Siamo orgogliosi di offrire tortellini di ottima qualità, realizzati dai nostri artigiani con una cura e un'attenzione particolari per la qualità. Se hai domande, richieste o desideri contattarci per qualsiasi motivo, sei nel posto giusto.
+    </p>
+    <br>
+    <p class="fw-semibold">
+        Informazioni di Contatto: 
+    </p>
+    <p class="fs-4">
+    Indirizzo: Via del Tortellino, 123, 00100 Roma, Italia <br>
+    Telefono: +39 123456789 <br>
+    Email: info@aziendatortelliniroma.it <br>
+    Account Github: <br>
+    Christian Angileri -> <a target="_blank" class="text-decoration-underline" href="https://github.com/Chri2704">https://github.com/Chri2704</a> <br>
+    Alessio Cassieri -> <a target="_blank" class="text-decoration-underline" href="https://github.com/Cassio7">https://github.com/Cassio7</a>
+    </p>
+    <br>
+    <p class="fw-semibold">
+    Perché scegliere i nostri tortellini?
+    </p>
+    <p>
+    I nostri tortellini sono realizzati con passione e dedizione dai nostri abili artigiani, 
+    che tramandano l'arte della produzione di pasta da generazioni. Ci impegniamo a garantire un'accuratezza assoluta nella qualità dei nostri prodotti,
+    utilizzando solo gli ingredienti più freschi e selezionati. Ecco un video dimostrazione: <br> <br>
+    </p>
+</div>
+
+<div style=" display: flex; justify-content: center;">  
+<video width="300" height="800" controls  style="border: 2px solid #000000; border-radius: 30px;"> 
+  <source src="/video/Tad&Apd.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+</video>
+</div>
+
