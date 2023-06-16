df5f5ada0197628b62320129ab854efd3112bd82
Cassio7 <acassieri@gmail.com>
2023-06-16 16:05:23 +0200

    i

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,23 +8,25 @@
         margin-top: auto;
         border-style: hidden;
     }
+
     table td {
         padding: 15px;
         border: 3px solid;
-      }
+    }
+
     .icons {
-        width: 300px;
-        height: 300px;
+        width: 245px;
+        height: 245px;
     }
     </style>
     <table>
-      <tr>
-        <td><a href="admin/userslist"><img class="icons" src="images/icons/users.png" alt="users"></a></td>
-        <td><a href="admin/modprod"><img class="icons" src="images/icons/items.png" alt="users"></a></td>
-      </tr>
-      <tr>
-      <td><a href="admin/newprod"><img class="icons" src="images/icons/new.png" alt="users"></a></td>
-      <td><a href="admin/orders"><img class="icons" src="images/icons/orders.png" alt="Ordini fatti?!"></a></td>
-      </tr>
+        <tr>
+            <td><a href="admin/userslist"><img class="icons" src="images/icons/users.png" alt="users"></a></td>
+            <td><a href="admin/modprod"><img class="icons" src="images/icons/items.png" alt="users"></a></td>
+        </tr>
+        <tr>
+            <td><a href="admin/newprod"><img class="icons" src="images/icons/new.png" alt="users"></a></td>
+            <td><a href="admin/orders"><img class="icons" src="images/icons/orders.png" alt="Ordini fatti?!"></a></td>
+        </tr>
     </table>
 </x-app-layout>
