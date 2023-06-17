b97b039bbec656f1ff82b5ebd554117c2977aabc
Chri2704 <angilerichristian439@gmail.com>
2023-06-17 17:36:13 +0200

    formattazione catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -8,8 +8,7 @@
 
 .centerimg {
     display: block;
-    margin-left: auto;
-    margin-right: auto;
+
     width: 200px;
     height: 200px;
 }
@@ -21,36 +20,54 @@
 @endif
 <h1 class="fs-1" style="text-align: center;">Catalogo prodotti disponibili</h1>
 <br>
-<div class="center">
+<div class="row float-center" style="margin-left: 5%; margin-right: 5%;">
     <!-- permette di stampare i prodotti passati dal controller con un for -->
     @foreach($products as $product)
     <!-- form che passa i dati al controller Homecontroller tramite post -->
-    <form action="{{url('new_order')}}" method="POST" enctype="multipart/form-data">
+    <div class="col-4 ">
+        <div class="coloreDiv">
+        <form action="{{url('new_order')}}" method="POST" enctype="multipart/form-data">
         @csrf
         <!-- serve per passare id del prodotto alla query nel controller -->
         <input type="hidden" name="product_id" value="{{ $product['id']}}">
         <br>
-        <h2>{{$product['id']}}</h2>
+        <img src="{{ asset('images/db/'.$product->image)}}" alt="Immagine" class="centerimg float-left" style="margin-left: 10px; border-radius: 5px">
+        <label for="" class="boldi fs-4" style="margin-left: 10px;">
         {{$product['nome_prodotto']}}
+        </label>
+        
         <br>
-        {{$product['descrizione']}}
+        <label for="" style="margin-left: 10px;">{{$product['descrizione']}}</label>
+        
+        <br>
 
-        <img src="{{ asset('images/db/'.$product->image)}}" alt="Immagine" class="centerimg">
-        <select name="quantity">
-            <option value="1" selected>1</option>
-            <!-- for che va fino a 10 per il numero prodotti -->
-            @for ($i=2 ; $i<=10 ; $i++)
-                <option value="{{$i}}">{{$i}}</option>
-            @endfor
-        </select>
-        <!-- controlla se utente è loggato, se si permette ordine senno login -->
-        @if (Route::has('login'))
-        @auth
-        <button type="submit" class="btn btn-primary bg-primary">Acquista</button>
+        <label for="" class="fs-3" style="margin-left: 10px;">
+               € {{$product['prezzo']}} 
+        </label>
+        <div class="row center">
+            <div class="col-md-3">
+                <select name="quantity" class="form-select ">
+                    <option value="1" selected>1</option>
+                    <!-- for che va fino a 10 per il numero prodotti -->
+                    @for ($i=2 ; $i<=10 ; $i++)
+                    <option value="{{$i}}">{{$i}}</option>
+                    @endfor
+                </select>
+            </div>
+            <!-- controlla se utente è loggato, se si permette ordine senno login -->
+            @if (Route::has('login'))
+            @auth
+            <div class="col-md-6 float-right">
+                <button type="submit" class="btn btn-primary bg-primary">Acquista</button>
+            </div>
+        </div>
+        
         @else
         <a class="btn btn-success" href="{{route('login')}}">Login per acquistare</a>
         @endauth
         @endif
     </form>
+    </div>
+    </div>
     @endforeach
 </div>
